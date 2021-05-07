884e5cce7ed7642fa986083cf18a90258430e489
rosshpayne <rosshpayne@gmail.com>
2021-05-07 02:00:05 +0000

    document pkg

---
+++ document/doc.go
@@ -0,0 +1,15 @@
+package doc
+
+import (
+	"github.com/rosshpayne/graph-sdl/internal/db"
+)
+
+var NoItemFoundErr = db.NoItemFoundErr
+
+func GetDocument() string {
+	return db.GetDocument()
+}
+
+func SetDocument(doc string) string {
+	return db.SetDocument(doc)
+}
